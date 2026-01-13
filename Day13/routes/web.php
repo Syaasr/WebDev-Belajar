667f23ddc3ab571b6f4715d485cceecbf89e2c0a
Syaasr <maulana23firdaus@gmail.com>
2026-01-13 14:19:14 +0700

    day 13 database migration and seeder

---
+++ Day13/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
+
+// 1. Route Home (/)
+Route::get('/', [HomeController::class, 'index']);
+
+// 2. Route Products (/products)
+Route::get('/products', [ProductController::class, 'index']);
+
+// 3. Route Cart (/cart)
+Route::get('/cart', [CartController::class, 'index']);
+
+// 4. Route Checkout (/checkout)
+Route::get('/checkout', [OrderController::class, 'checkout']);
